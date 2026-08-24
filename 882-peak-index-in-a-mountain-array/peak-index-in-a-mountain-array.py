39f66563c68a43e6d4d2ffde6331cd115c0e2bae
Aviral Pandey <aviralp195@gmail.com>
2026-08-24 20:03:38 +0530

    Time: 24 ms (5.12%) | Memory: 21.1 MB (98.86%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def peakIndexInMountainArray(self, arr):
+        max_val = arr[0]
+        index = 0
+
+        for i in range(len(arr)):
+            if arr[i] > max_val:
+                max_val = arr[i]
+                index = i
+
+        return index
+ 
+
+       
+
+
+
+       
+        
